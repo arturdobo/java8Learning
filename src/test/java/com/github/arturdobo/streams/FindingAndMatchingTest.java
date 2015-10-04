49713ba19b42cd5a3171c300e662688ed5ad6b98
dobo <arturdobo@gmail.com>
2015-10-04 17:38:34 +0200

    added to finding and matching

--- src/test/java/com/github/arturdobo/streams/FindingAndMatchingTest.java
+++ src/test/java/com/github/arturdobo/streams/FindingAndMatchingTest.java
@@ -5,7 +5,11 @@ import com.github.arturdobo.trade.TradeRepo;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Optional;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.core.IsEqual.*;
@@ -38,10 +42,33 @@ public class FindingAndMatchingTest {
 		Optional<String> string = Arrays.asList("Aaaaaa", "Bbbbbbb", "Ccc")
 		                                .stream()
 		                                .filter(s -> s.length() > 5)
-		                                .findAny();
+		                                .findAny(); // it could be findFirst. Difference in parallel
 
 		assertThat(string.isPresent(), is(true));
 		assertThat(string.get(), equalTo("Aaaaaa"));
 	}
+	
+	@Test
+	public void findAnyVsFindFistInSortedCollection() throws Exception {
+		SortedSet<Integer> ints = new TreeSet<>(Arrays.asList(7, 2, 9, 4, 6, 8, 1));
 
+		assertThat(ints.stream()
+		               .findFirst()
+		               .get(),
+		           equalTo(ints.stream()
+		                       .findAny()
+		                       .get()));
+	}
+
+	@Test
+	public void findAnyVsFindFistInUnsortedCollection() throws Exception {
+		Set<String> ints = new HashSet<>(Arrays.asList("zzz", "dd", "ddd", "ee", "qq", "a", "xxx"));
+
+		assertThat(ints.stream()
+		               .findFirst()
+		               .get(),
+		           equalTo(ints.stream()
+		                       .findAny()
+		                       .get()));
+	}
 }
